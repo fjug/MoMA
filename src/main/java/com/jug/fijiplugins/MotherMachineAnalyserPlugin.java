858d0c5096b87591d4c6e8b8abb8824a8079d6d9
haesleinhuepf <rhaase@mpi-cbg.de>
2016-10-21 10:58:52 +0200

    added a dialog to be generically usable

--- src/main/java/com/jug/fijiplugins/MotherMachineAnalyserPlugin.java
+++ src/main/java/com/jug/fijiplugins/MotherMachineAnalyserPlugin.java
@@ -1,9 +1,12 @@
 package com.jug.fijiplugins;
 
 import com.jug.MoMA;
+import fiji.util.gui.GenericDialogPlus;
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
+import ij.Prefs;
+import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
@@ -27,15 +30,39 @@ public class MotherMachineAnalyserPlugin implements PlugIn {
 
     @Override
     public void run(String s) {
+        String currentDir = Prefs.getDefaultDirectory();
+
+        GenericDialogPlus gd = new GenericDialogPlus("MoMA configuration");
+        gd.addDirectoryField("Input_folder", currentDir);
+        gd.addDirectoryField("Output_folder", currentDir);
+        gd.addNumericField("Number_of_Channels", 2, 0);
+        gd.showDialog();
+        if (gd.wasCanceled()) {
+            return;
+        }
+        String inputFolder = gd.getNextString();
+        String outputFolder = gd.getNextString();
+        int numberOfChannels = (int)gd.getNextNumber();
+
         IJ.log("Hello Mother!");
 
-        String[] args = new String[5];
+        String[] args = {
+                "moma",
+                "-i",
+                inputFolder,
+                "-o",
+                outputFolder,
+                "-c",
+                "" + numberOfChannels
+        };
+                /*new String[5];
         args[0] = "moma";
         args[1] = "-i";
         args[2] = "/Users/rhaase/Projects/Florian_Jug_Myers_MoMA_Deployment/data/MoMA_example";
         args[3] = "-c";
-        args[4] = "2";
+        args[4] = "2";*/
 
+        MoMA.running_as_Fiji_plugin = true;
         MoMA.main(args);
     }
 
