afcad2cd3d1ec2384dff09060145c9c5fc245f66
haesleinhuepf <rhaase@mpi-cbg.de>
2016-10-24 16:17:20 +0200

    change version display inside the tool

--- src/main/java/com/jug/MoMA.java
+++ src/main/java/com/jug/MoMA.java
@@ -85,7 +85,7 @@ public class MoMA {
 	/**
 	 * Identifier of current version
 	 */
-	public static final String VERSION_STRING = "MoMA_0.9.6";
+	public static final String VERSION_STRING = "MoMA_0.10.0";
 
 	// -------------------------------------------------------------------------------------
 	// statics
