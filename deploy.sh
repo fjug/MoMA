b23bb292b6b066fb4a58933319798d13b1231d1e
random <hallo@frauzufall.de>
2017-06-12 16:23:21 +0200

    remove enforcer skip from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ mvn install:install-file -Dfile=lib/jmathplot.jar -DgroupId=jmathplot -Dartifact
 
 mvn install:install-file -Dfile=lib/gurobi.jar -DgroupId=gurobi -DartifactId=gurobi -Dversion=1.0 -Dpackaging=jar
 
-mvn -Denforcer.skip package
+mvn package
